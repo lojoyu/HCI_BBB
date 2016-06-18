a2ed4bb36d1faea8504bbb484d1cec570f3d1c33
Mu <wenny820622@gmail.com>
2016-06-18 23:16:28 +0800

    6/18 Google Street View click button "show location" will switch to map view, it include google map and street view and two button to call users car.

--- app/src/main/java/app/hcicourse/bbb/gps/MapsActivity.java
+++ app/src/main/java/app/hcicourse/bbb/gps/MapsActivity.java
@@ -2,14 +2,20 @@ package app.hcicourse.bbb.gps;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.net.Uri;
 import android.content.Intent;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.support.v4.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.StreetViewPanorama;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.OnStreetViewPanoramaReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.StreetViewPanoramaFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Marker;
@@ -20,8 +26,10 @@ import app.hcicourse.bbb.R;
  * Created by Wolf on 2016/6/18.
  */
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-    Double latitude, longitude;
+    private Double latitude, longitude;
     private GoogleMap googleMap;
+    private WebView webView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +42,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+//        StreetViewPanoramaFragment streetViewPanoramaFragment = (StreetViewPanoramaFragment) getFragmentManager().findFragmentById(R.id.streetviewpanorama);
+//        StreetViewPanorama panorama = streetViewPanoramaFragment.getStreetViewPanorama();
+//        panorama.setPosition(new LatLng(latitude, longitude));
+        mappingWidgets();
+    }
+
+    private void mappingWidgets() {
+
+        webView = (WebView) findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.getSettings().setSupportZoom(false);
+
+        // If you want to load it from assets (you can customize it if you want)
+        Uri uri = Uri.parse("file:///android_asset/streetviewscript.html");
+        // If you want to load it directly
+        //Uri uri = Uri.parse("https://google-developers.appspot.com/maps/documentation/javascript/examples/full/streetview-simple");
+        webView.loadUrl(uri.toString());
+        webView.addJavascriptInterface(new JavaScriptInterface(this), "Android");
+        webView.setWebViewClient(new WebViewClient(){
+            public void onPageFinished(WebView view, String url){
+                webView.loadUrl("javascript:initialize(" + latitude +"," + longitude + ")");
+            }
+        });
     }
 
     @Override
@@ -41,7 +76,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Add a marker in Sydney, Australia, and move the camera.
         LatLng location = new LatLng(latitude, longitude);
         map.addMarker(new MarkerOptions().position(location).title("Current Location"));
-        map.moveCamera(CameraUpdateFactory.newLatLng(location));
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(location, 18), 200, null);
+        //map.animateCamera(CameraUpdateFactory.zoomTo(17), 200, null);
+        //map.moveCamera(CameraUpdateFactory.newLatLng(location));
     }
-
 }
